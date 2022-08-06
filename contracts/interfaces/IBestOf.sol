a7e3b412c325bf73c2028a5ce9e152078bbb0fb0
Tim Pechersky <tim.pechersky@gmail.com>
2022-08-06 22:06:45 +0200

    remove requirement interfaces from IBestOf

--- contracts/interfaces/IBestOf.sol
+++ contracts/interfaces/IBestOf.sol
@@ -4,34 +4,7 @@ import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import {LibTBG} from "../libraries/LibTurnBasedGame.sol";
 
 interface IBestOf {
-    enum TokenTypes {
-        NATIVE,
-        ERC20,
-        ERC1155,
-        ERC721
-    }
 
-    enum TokenMust {
-        HAVE,
-        LOCK,
-        BURN,
-        MINT,
-        BET,
-        GIVE
-    }
-
-    struct Token {
-        TokenTypes tokenType;
-        address tokenAddress;
-        uint256 tokenId;
-    }
-
-    struct TokenAction {
-        Token token;
-        uint256 amount;
-        bool requireParticularERC721;
-        TokenMust must;
-    }
 
     struct Score {
         address participant;
