5919d930c2aed1153f4b06880dc410ccc3916208
Tim Pechersky <tim.pechersky@gmail.com>
2022-08-11 20:36:58 +0200

    prettify

--- contracts/mocks/MockERC1155.sol
+++ contracts/mocks/MockERC1155.sol
@@ -4,31 +4,42 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.8.0;
 
-contract MockERC1155 is ERC1155, Ownable
-{
-    constructor(string memory uri_, address owner) ERC1155(uri_)
-    {
+contract MockERC1155 is ERC1155, Ownable {
+    constructor(string memory uri_, address owner) ERC1155(uri_) {
         require(owner != address(0), "must specify owner of the contract");
         transferOwnership(owner);
     }
 
-    function mint(address to, uint256 amount, uint256 poolId, bytes memory data) public onlyOwner {
+    function mint(
+        address to,
+        uint256 amount,
+        uint256 poolId,
+        bytes memory data
+    ) public onlyOwner {
         require(to != address(0), "MockERC1155->mint: Address not specified");
-        require(amount != 0,  "MockERC1155->mint: amount not specified");
-        require(poolId != 0,  "MockERC1155->mint: pool id not specified");
-        _mint(to, poolId,amount, data);
+        require(amount != 0, "MockERC1155->mint: amount not specified");
+        require(poolId != 0, "MockERC1155->mint: pool id not specified");
+        _mint(to, poolId, amount, data);
     }
 
-    function batchMint(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner
-    {
+    function batchMint(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
         require(to != address(0), "MockERC1155->batchMint: Address not specified");
-        require(amounts.length != 0,  "MockERC1155->batchMint: amount not specified");
-        require(ids.length != 0,  "MockERC1155->batchMint: pool id not specified");
-        _mintBatch(to,ids,amounts,data);
+        require(amounts.length != 0, "MockERC1155->batchMint: amount not specified");
+        require(ids.length != 0, "MockERC1155->batchMint: pool id not specified");
+        _mintBatch(to, ids, amounts, data);
     }
-    function levelUp(address to, uint256 id, bytes memory data) public onlyOwner
-    {
-        _burn(to, id,1);
-        _mint(to,id+1,1,data);
+
+    function levelUp(
+        address to,
+        uint256 id,
+        bytes memory data
+    ) public onlyOwner {
+        _burn(to, id, 1);
+        _mint(to, id + 1, 1, data);
     }
 }
