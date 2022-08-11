d46766510a22f35df0cdb09bfbb4fe0c6e41d978
Tim Pechersky <tim.pechersky@gmail.com>
2022-08-11 13:57:00 +0200

    mock contract to test LibCoinVending

---
+++ contracts/mocks/MockVendingMachine.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+// Author: Tim Pechersky <@Peersky>
+
+pragma solidity ^0.8.4;
+
+import {LibCoinVending} from "../libraries/LibCoinVending.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "hardhat/console.sol";
+
+contract MockVendingMachine is ReentrancyGuard {
+    constructor() {}
+
+    address[] public participants;
+
+    function createPosition(string memory position, LibCoinVending.ConfigPosition memory configuration) public {
+        LibCoinVending.configure(position, configuration);
+    }
+
+    function fund(string memory _positionName) public payable nonReentrant {
+        LibCoinVending.fund(_positionName);
+    }
+
+    function release(
+        string memory _positionName,
+        address payee,
+        address beneficiary
+    ) public nonReentrant {
+        LibCoinVending.release(_positionName, payee, beneficiary, msg.sender);
+    }
+
+    function refund(string memory _positionName, address to) public nonReentrant {
+        LibCoinVending.refund(_positionName, to);
+    }
+
+    function refundBatch(string memory _positionName) public nonReentrant {
+        LibCoinVending.batchRefund(_positionName, participants);
+    }
+
+    function releaseAll(
+        string memory _positionName,
+        address payee,
+        address beneficiary
+    ) public nonReentrant {
+        LibCoinVending.batchRelease(_positionName, payee, beneficiary, participants);
+    }
+
+    function onERC1155Received(
+        address operator,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) public view returns (bytes4) {
+        if (operator == address(this)) {
+            return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+        }
+        return bytes4("");
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external view returns (bytes4) {
+        if (operator == address(this)) {
+            return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+        }
+        return bytes4("");
+    }
+
+    function onERC721Received(
+        address operator,
+        address,
+        uint256,
+        bytes calldata
+    ) external view returns (bytes4) {
+        if (operator == address(this)) {
+            return IERC721Receiver.onERC721Received.selector;
+        }
+        return bytes4("");
+    }
+}
