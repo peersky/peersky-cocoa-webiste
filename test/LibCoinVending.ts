a08911bfcbccb16d94ad331c169aa427d6e99e05
Tim Pechersky <tim.pechersky@gmail.com>
2022-08-11 13:56:26 +0200

    coin acceptor library

---
+++ test/LibCoinVending.ts
@@ -0,0 +1,498 @@
+const { assert } = require("chai");
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import {
+  MockERC1155,
+  MockVendingMachine,
+  MockERC20,
+  MockERC721,
+} from "../types/typechain/contracts/mocks";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { LibCoinVending } from "../types/typechain/contracts/mocks/MockVendingMachine";
+
+const eth0 = ethers.utils.parseEther("0");
+const eth1 = ethers.utils.parseEther("0.1");
+const eth2 = ethers.utils.parseEther("0.2");
+const eth3 = ethers.utils.parseEther("0.3");
+const eth4 = ethers.utils.parseEther("0.4");
+const eth5 = ethers.utils.parseEther("0.5");
+const eth10 = ethers.utils.parseEther("1");
+const eth100 = ethers.utils.parseEther("1000");
+const valueToHave = eth5;
+const valueToLock = eth1;
+const valueToBurn = eth2;
+const valueToAward = eth3;
+const valueToAccept = eth4;
+
+describe("LibCoinVending Test", async function () {
+  let mockCoinVending: MockVendingMachine;
+  let signer: SignerWithAddress;
+  let payee: SignerWithAddress;
+  let maliciousActor: SignerWithAddress;
+  let benificiary: SignerWithAddress;
+  let mockERC1155: MockERC1155;
+  let mockERC20: MockERC20;
+  let mockERC721: MockERC721;
+  let ReqTokens: LibCoinVending.TokenConfigurationStruct[] = [];
+
+  beforeEach(async function () {
+    const signers = await ethers.getSigners();
+    signer = signers[0];
+    payee = signers[1];
+    benificiary = signers[2];
+    maliciousActor = signers[3];
+
+    const MockCoinVending = await ethers.getContractFactory(
+      "MockVendingMachine"
+    );
+    const _mockCoinVending =
+      (await MockCoinVending.deploy()) as MockVendingMachine;
+    await _mockCoinVending.deployed();
+
+    const MockERC20 = await ethers.getContractFactory("MockERC20");
+    const _mockERC20 = (await MockERC20.deploy(
+      "ERC20",
+      "ERC20",
+      signer.address
+    )) as MockERC20;
+    await _mockERC20.deployed();
+    await _mockERC20.mint(signer.address, eth100);
+    await _mockERC20.increaseAllowance(_mockCoinVending.address, eth100);
+    await _mockERC20
+      .connect(maliciousActor)
+      .increaseAllowance(_mockCoinVending.address, eth100);
+
+    const MockERC721 = await ethers.getContractFactory("MockERC721");
+    const _mockERC721 = (await MockERC721.deploy(
+      "ERC721",
+      "ERC721",
+      signer.address
+    )) as MockERC721;
+    await _mockERC721.deployed();
+
+    for (let i = 1; i < 100; i++) {
+      await _mockERC721.mintNext(signer.address);
+    }
+    await _mockERC721.setApprovalForAll(_mockCoinVending.address, true);
+    await _mockERC721
+      .connect(maliciousActor)
+      .setApprovalForAll(_mockCoinVending.address, true);
+
+    const MockERC1155 = await ethers.getContractFactory("MockERC1155");
+    const _mockERC1155 = (await MockERC1155.deploy(
+      " ",
+      signer.address
+    )) as MockERC1155;
+    await _mockERC1155.deployed();
+    await _mockERC1155.setApprovalForAll(_mockCoinVending.address, true);
+    await _mockERC1155
+      .connect(maliciousActor)
+      .setApprovalForAll(_mockCoinVending.address, true);
+
+    await _mockERC1155.batchMint(
+      signer.address,
+      ["1", "2", "3", "4", "5", "6"],
+      [eth100, eth100, eth100, eth100, eth100, eth100],
+      "0x"
+    );
+    ReqTokens = [];
+    //ERC20
+    ReqTokens.push({
+      tokenAddress: _mockERC20.address,
+      tokenId: "0",
+      tokenReq: {
+        valueToHave: valueToHave,
+        valueToLock: valueToLock,
+        valueToBurn: valueToBurn,
+        valueToAward: valueToAward,
+        valueToAccept: valueToAccept,
+        _type: "0",
+        own712Id: "0",
+      },
+    });
+
+    //ERC1155
+    ReqTokens.push({
+      tokenAddress: _mockERC1155.address,
+      tokenId: "1",
+      tokenReq: {
+        valueToHave: valueToHave,
+        valueToLock: valueToLock,
+        valueToBurn: valueToBurn,
+        valueToAward: valueToAward,
+        valueToAccept: valueToAccept,
+        _type: "1",
+        own712Id: "0",
+      },
+    });
+
+    //ERC721 have (balance)
+    ReqTokens.push({
+      tokenAddress: _mockERC721.address,
+      tokenId: "1",
+      tokenReq: {
+        valueToHave: "1",
+        valueToLock: "0",
+        valueToBurn: "0",
+        valueToAward: "0",
+        valueToAccept: "0",
+        _type: "2",
+        own712Id: "0",
+      },
+    });
+
+    //ERC721 have (tokenId)
+    ReqTokens.push({
+      tokenAddress: _mockERC721.address,
+      tokenId: "2",
+      tokenReq: {
+        valueToHave: "0",
+        valueToLock: "0",
+        valueToBurn: "0",
+        valueToAward: "0",
+        valueToAccept: "0",
+        _type: "2",
+        own712Id: "1",
+      },
+    });
+
+    //ERC721 lock
+    ReqTokens.push({
+      tokenAddress: _mockERC721.address,
+      tokenId: "3",
+      tokenReq: {
+        valueToHave: "0",
+        valueToLock: "1",
+        valueToBurn: "0",
+        valueToAward: "0",
+        valueToAccept: "0",
+        _type: "2",
+        own712Id: "0",
+      },
+    });
+
+    //ERC721 burn
+    ReqTokens.push({
+      tokenAddress: _mockERC721.address,
+      tokenId: "4",
+      tokenReq: {
+        valueToHave: "0",
+        valueToLock: "0",
+        valueToBurn: "1",
+        valueToAward: "0",
+        valueToAccept: "0",
+        _type: "2",
+        own712Id: "0",
+      },
+    });
+
+    //ERC721 award
+    ReqTokens.push({
+      tokenAddress: _mockERC721.address,
+      tokenId: "5",
+      tokenReq: {
+        valueToHave: "0",
+        valueToLock: "0",
+        valueToBurn: "0",
+        valueToAward: "1",
+        valueToAccept: "0",
+        _type: "2",
+        own712Id: "0",
+      },
+    });
+
+    //ERC721 accept
+    ReqTokens.push({
+      tokenAddress: _mockERC721.address,
+      tokenId: "6",
+      tokenReq: {
+        valueToHave: "0",
+        valueToLock: "0",
+        valueToBurn: "0",
+        valueToAward: "0",
+        valueToAccept: "1",
+        _type: "2",
+        own712Id: "0",
+      },
+    });
+
+    mockERC20 = _mockERC20;
+    mockERC721 = _mockERC721;
+    mockERC1155 = _mockERC1155;
+    mockCoinVending = _mockCoinVending;
+  });
+
+  it("Should be able to create new position without tokens", async () => {
+    await expect(
+      mockCoinVending.createPosition("test position", {
+        valueToHave: valueToHave,
+        valueToLock: valueToLock,
+        valueToBurn: valueToBurn,
+        valueToAward: valueToAward,
+        valueToAccept: valueToAccept,
+        tokenConfigs: [],
+      })
+    ).not.to.be.reverted;
+  });
+  it("Should revert on interaction with non exsistent positions", async () => {
+    await expect(
+      mockCoinVending.fund("nonExistentPosition", { value: eth1 })
+    ).to.be.revertedWith("Position does not exist");
+    await expect(
+      mockCoinVending.refund("nonExistentPosition", signer.address)
+    ).to.be.revertedWith("Not enough balance to refund");
+    await expect(
+      mockCoinVending.release(
+        "nonExistentPosition",
+        payee.address,
+        benificiary.address
+      )
+    ).to.be.revertedWith("Not enough balance to release");
+  });
+  describe("When position without tokens created", () => {
+    beforeEach(async () => {
+      const _req: LibCoinVending.ConfigPositionStruct = {
+        valueToHave: valueToHave,
+        valueToLock: valueToLock,
+        valueToBurn: valueToBurn,
+        valueToAward: valueToAward,
+        valueToAccept: valueToAccept,
+        tokenConfigs: [],
+      };
+      await mockCoinVending.createPosition("test position1", _req);
+    });
+    it("Allows to fund with correct value", async () => {
+      await expect(mockCoinVending.fund("test position1", { value: eth10 })).not
+        .to.be.reverted;
+    });
+    it("Reverts attempt to fund with not enough value", async () => {
+      await expect(
+        mockCoinVending.fund("test position1", { value: eth5 })
+      ).to.be.revertedWith("msg.value too low");
+    });
+    it("Reverts attempt to refund", async () => {
+      await expect(
+        mockCoinVending.refund("test position1", signer.address)
+      ).to.be.revertedWith("Not enough balance to refund");
+    });
+    it("Reverts attempt to release", async () => {
+      await expect(
+        mockCoinVending.release(
+          "test position1",
+          signer.address,
+          signer.address
+        )
+      ).to.be.revertedWith("Not enough balance to release");
+    });
+    it("Funding takes away proper value and Refunded address gets same balance as before funding", async () => {
+      const initialBalance = await ethers.provider.getBalance(signer.address);
+
+      let tx = await mockCoinVending.fund("test position1", { value: eth10 });
+      let txReceipt = await tx.wait();
+      let gasSpent = txReceipt.gasUsed.mul(txReceipt.effectiveGasPrice);
+      let updatedBalance = await ethers.provider.getBalance(signer.address);
+      expect(initialBalance).to.be.equal(
+        updatedBalance.add(gasSpent).add(eth10)
+      );
+      tx = await mockCoinVending.refund("test position1", signer.address);
+      txReceipt = await tx.wait();
+      let gasSpent2 = txReceipt.gasUsed.mul(txReceipt.effectiveGasPrice);
+      updatedBalance = await ethers.provider.getBalance(signer.address);
+      expect(initialBalance).to.be.equal(
+        updatedBalance.add(gasSpent).add(gasSpent2)
+      );
+    });
+    it("Release brings correct values back to funder, benificiary and payee", async () => {
+      const initialBalance = await ethers.provider.getBalance(signer.address);
+      const initialPayeeBalance = await ethers.provider.getBalance(
+        payee.address
+      );
+      const initialBenificiaryBalance = await ethers.provider.getBalance(
+        benificiary.address
+      );
+
+      let tx = await mockCoinVending.fund("test position1", { value: eth10 });
+      let txReceipt = await tx.wait();
+      let gasSpent = txReceipt.gasUsed.mul(txReceipt.effectiveGasPrice);
+      let updatedBalance = await ethers.provider.getBalance(signer.address);
+      expect(initialBalance).to.be.equal(
+        updatedBalance.add(gasSpent).add(eth10)
+      );
+      tx = await mockCoinVending.release(
+        "test position1",
+        payee.address,
+        benificiary.address
+      );
+      txReceipt = await tx.wait();
+      let gasSpent2 = txReceipt.gasUsed.mul(txReceipt.effectiveGasPrice);
+      updatedBalance = await ethers.provider.getBalance(signer.address);
+      const payeeBalance = await ethers.provider.getBalance(payee.address);
+      const benificiaryBalance = await ethers.provider.getBalance(
+        benificiary.address
+      );
+      expect(
+        initialBalance.sub(valueToAccept).sub(valueToAward).sub(valueToBurn)
+      ).to.be.equal(updatedBalance.add(gasSpent).add(gasSpent2));
+      expect(benificiaryBalance).to.be.equal(
+        initialBenificiaryBalance.add(valueToAward)
+      );
+      expect(payeeBalance).to.be.equal(initialPayeeBalance.add(valueToAccept));
+    });
+  });
+  describe("When position with custom tokens is implemented", () => {
+    beforeEach(async () => {
+      const _req: LibCoinVending.ConfigPositionStruct = {
+        valueToHave: "0",
+        valueToLock: "0",
+        valueToBurn: "0",
+        valueToAward: "0",
+        valueToAccept: "0",
+        tokenConfigs: ReqTokens,
+      };
+      await mockCoinVending.createPosition("tokens", _req);
+    });
+    it("Takes all required tokens in fund stage", async () => {
+      await mockCoinVending.fund("tokens");
+    });
+    it("Reverts attempt to fund with not enough tokens", async () => {
+      await expect(
+        mockCoinVending.connect(maliciousActor).fund("tokens")
+      ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+      const balanceERC20 = await mockERC20.balanceOf(signer.address);
+      await mockERC20.transfer(maliciousActor.address, balanceERC20);
+      await expect(
+        mockCoinVending.connect(maliciousActor).fund("tokens")
+      ).to.be.revertedWith("ERC1155 balance is not valid");
+      const balanceERC1155 = await mockERC1155.balanceOf(signer.address, "1");
+      await mockERC1155.safeTransferFrom(
+        signer.address,
+        maliciousActor.address,
+        "1",
+        balanceERC1155,
+        "0x"
+      );
+      await expect(
+        mockCoinVending.connect(maliciousActor).fund("tokens")
+      ).to.be.revertedWith("Not enough ERC721 balance");
+      await mockERC721["safeTransferFrom(address,address,uint256)"](
+        signer.address,
+        maliciousActor.address,
+        "1"
+      );
+      await expect(
+        mockCoinVending.connect(maliciousActor).fund("tokens")
+      ).to.be.revertedWith("ERC721: transfer from incorrect owner");
+      await mockERC721["safeTransferFrom(address,address,uint256)"](
+        signer.address,
+        maliciousActor.address,
+        "2"
+      );
+      await expect(
+        mockCoinVending.connect(maliciousActor).fund("tokens")
+      ).to.be.revertedWith("ERC721: transfer from incorrect owner");
+      await mockERC721["safeTransferFrom(address,address,uint256)"](
+        signer.address,
+        maliciousActor.address,
+        "3"
+      );
+      await expect(
+        mockCoinVending.connect(maliciousActor).fund("tokens")
+      ).to.be.revertedWith("ERC721: transfer from incorrect owner");
+      await mockERC721["safeTransferFrom(address,address,uint256)"](
+        signer.address,
+        maliciousActor.address,
+        "4"
+      );
+      await expect(
+        mockCoinVending.connect(maliciousActor).fund("tokens")
+      ).to.be.revertedWith("ERC721: transfer from incorrect owner");
+      await mockERC721["safeTransferFrom(address,address,uint256)"](
+        signer.address,
+        maliciousActor.address,
+        "5"
+      );
+      await expect(
+        mockCoinVending.connect(maliciousActor).fund("tokens")
+      ).to.be.revertedWith("ERC721: transfer from incorrect owner");
+      await mockERC721["safeTransferFrom(address,address,uint256)"](
+        signer.address,
+        maliciousActor.address,
+        "6"
+      );
+      await expect(mockCoinVending.connect(maliciousActor).fund("tokens")).to
+        .not.be.reverted;
+    });
+    it("Reverts attempt to refund", async () => {
+      await expect(
+        mockCoinVending.refund("tokens", signer.address)
+      ).to.be.revertedWith("Not enough balance to refund");
+    });
+    it("Reverts attempt to release", async () => {
+      await expect(
+        mockCoinVending.release("tokens", signer.address, signer.address)
+      ).to.be.revertedWith("Not enough balance to release");
+    });
+    it("Funding takes away proper tokens and Refunded address getstokens back as before funding", async () => {
+      const erc20initialBalance = await mockERC20.balanceOf(signer.address);
+      const erc1155initialBalance = await mockERC1155.balanceOf(
+        signer.address,
+        "1"
+      );
+      await mockCoinVending.fund("tokens");
+
+      expect(await mockERC20.balanceOf(mockCoinVending.address)).to.be.equal(
+        eth10
+      );
+      expect(
+        await mockERC1155.balanceOf(mockCoinVending.address, "1")
+      ).to.be.equal(eth10);
+      expect(await mockERC721.ownerOf("1")).to.be.equal(signer.address);
+      expect(await mockERC721.ownerOf("2")).to.be.equal(signer.address);
+      expect(await mockERC721.ownerOf("3")).to.be.equal(
+        mockCoinVending.address
+      );
+      expect(await mockERC721.ownerOf("4")).to.be.equal(
+        mockCoinVending.address
+      );
+      expect(await mockERC721.ownerOf("5")).to.be.equal(
+        mockCoinVending.address
+      );
+      expect(await mockERC721.ownerOf("6")).to.be.equal(
+        mockCoinVending.address
+      );
+      await mockCoinVending.refund("tokens", signer.address);
+      expect(await mockERC20.balanceOf(signer.address)).to.be.equal(
+        erc20initialBalance
+      );
+      expect(await mockERC1155.balanceOf(signer.address, "1")).to.be.equal(
+        erc1155initialBalance
+      );
+      expect(await mockERC721.ownerOf("1")).to.be.equal(signer.address);
+      expect(await mockERC721.ownerOf("2")).to.be.equal(signer.address);
+      expect(await mockERC721.ownerOf("3")).to.be.equal(signer.address);
+      expect(await mockERC721.ownerOf("4")).to.be.equal(signer.address);
+      expect(await mockERC721.ownerOf("5")).to.be.equal(signer.address);
+      expect(await mockERC721.ownerOf("6")).to.be.equal(signer.address);
+    });
+    it("brings correct values upon Fund & Release back to funder, benificiary and payee", async () => {
+      await mockCoinVending.fund("tokens");
+      //   await mockCoinVending.release(
+      //     "tokens",
+      //     payee.address,
+      //     benificiary.address
+      //   );
+      //   expect(await mockERC20.balanceOf(benificiary.address)).to.be.equal(
+      //     valueToAward
+      //   );
+      //   expect(await mockERC20.balanceOf(payee.address)).to.be.equal(
+      //     valueToAccept
+      //   );
+      //   expect(await mockERC721.ownerOf("3")).to.be.equal(signer.address);
+      //   expect(await mockERC721.ownerOf("4")).to.be.equal(
+      //     ethers.constants.AddressZero
+      //   );
+      //   expect(await mockERC721.ownerOf("5")).to.be.equal(benificiary.address);
+      //   expect(await mockERC721.ownerOf("6")).to.be.equal(payee.address);
+    });
+  });
+});
