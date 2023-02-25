b17f1f9455a807e02e0d6383a50eed3172f200a4
Tim Pechersky <tim.pechersky@gmail.com>
2023-02-25 19:11:35 +0800

    blog initial added

--- apps/bestofweb/config.ts
+++ apps/bestofweb/config.ts
@@ -2,45 +2,48 @@ import { SiteMap, SiteMapItemType } from "@peersky/next-web3-chakra/types";
 
 export const SITEMAP: SiteMap = [
   {
-    title: "Tools",
+    title: "Blog",
+    path: "/blog",
+    type: SiteMapItemType.CONTENT,
+  },
+  {
+    title: "dApp",
+    path: "/dapp",
+    type: SiteMapItemType.CONTENT,
+  },
+  {
+    title: "Utils",
     path: "/tools",
-    type: SiteMapItemType.EMPTY,
-    children: [
-      {
-        title: "Contract R/W",
-        path: "/contracts",
-        type: SiteMapItemType.CONTENT,
-      },
-      {
-        title: "Hash calcualtor",
-        path: "/solihash",
-        type: SiteMapItemType.EXTERNAL,
-      },
-    ],
-  },
-  {
-    title: "Multipass",
-    path: "/multipass",
     type: SiteMapItemType.CONTENT,
+    // children: [
+    //   {
+    //     title: "Contract R/W",
+    //     path: "/contracts",
+    //     type: SiteMapItemType.CONTENT,
+    //   },
+    //   {
+    //     title: "Hash calcualtor",
+    //     path: "/solihash",
+    //     type: SiteMapItemType.EXTERNAL,
+    //   },
+    // ],
   },
-  {
-    title: "Game of best",
-    path: "/multipass",
-    type: SiteMapItemType.CONTENT,
-  },
-
   // {
-  //   title: "About",
-  //   path: "/about",
-  //   type: SiteMapItemType.EMPTY,
-  //   children: [
-  //     {
-  //       title: "Team",
-  //       path: "/team",
-  //       type: SiteMapItemType.CONTENT,
-  //     },
-  //   ],
+  //   title: "Multipass",
+  //   path: "/multipass",
+  //   type: SiteMapItemType.CONTENT,
   // },
+  // {
+  //   title: "Game of best",
+  //   path: "/multipass",
+  //   type: SiteMapItemType.CONTENT,
+  // },
+
+  {
+    title: "About",
+    path: "/about",
+    type: SiteMapItemType.CONTENT,
+  },
   {
     title: "Legal",
     path: "/legal",
---
+++ apps/bestofweb/pages/blog/index.tsx
@@ -0,0 +1,32 @@
+import { getLayout } from "@peersky/next-web3-chakra/layouts/BlogLayout";
+import { List, Flex, ListItem, Spacer, Stack, Text } from "@chakra-ui/react";
+import RouteButton from "@peersky/next-web3-chakra/components/RouteButton";
+// import Content from "../../content/test.mdx";
+
+const Blog = () => {
+  return (
+    <Flex w="100%">
+      {/* <ListItem w="100%"> */}
+      <Flex
+        dir="column"
+        w="100%"
+        borderWidth="2px"
+        borderRadius={"md"}
+        borderColor="ActiveCaption"
+        alignItems={"center"}
+        px={2}
+      >
+        <Text>Open Economy - Intro to money</Text>
+        <Spacer />
+        <Text>24 Feb 2023 </Text>
+        <Spacer />{" "}
+        <RouteButton variant="outline" href="/blog/OpenEconomy">
+          Open
+        </RouteButton>
+      </Flex>
+      {/* </ListItem> */}
+    </Flex>
+  );
+};
+Blog.getLayout = getLayout;
+export default Blog;
