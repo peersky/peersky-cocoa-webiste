18e631524f527d27169ff8bccab361dbd4050d22
Tim Pechersky <tim.pechersky@gmail.com>
2023-02-28 01:02:04 +0800

    add asset prefix

--- apps/bestofweb/next.config.js
+++ apps/bestofweb/next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const withMDX = require("@next/mdx")();
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   transpilePackages: ["@peersky/next-web3-chakra"],
   reactStrictMode: true,
   trailingSlash: true,
-
+  assetPrefix: isProd ? "https://peersky.github.io/daococoa" : undefined,
   // output: "standalone",
   // target: "serverless",
   webpack: (config, { isServer, webpack }) => {
