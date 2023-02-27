c350a4b94ca79bf8808536feb1b69fd5e913cac5
Tim Pechersky <tim.pechersky@gmail.com>
2023-02-27 23:59:25 +0800

    ci cd

--- apps/bestofweb/next.config.js
+++ apps/bestofweb/next.config.js
@@ -1,34 +1,30 @@
 /** @type {import('next').NextConfig} */
 const withMDX = require("@next/mdx")();
-module.exports = withMDX(
-  {
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-    transpilePackages: ["@peersky/next-web3-chakra"],
-    reactStrictMode: true,
-    trailingSlash: true,
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  transpilePackages: ["@peersky/next-web3-chakra"],
+  reactStrictMode: true,
+  trailingSlash: true,
 
-    // output: "standalone",
-    // target: "serverless",
-    webpack: (config, { isServer, webpack }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        // config.node = { fs: 'empty' };
-        config.resolve.fallback.fs = false;
-        config.resolve.fallback.electron = false;
-      }
-      config.plugins.push(
-        new webpack.IgnorePlugin({
-          resourceRegExp: /^electron$/,
-        })
-      );
+  // output: "standalone",
+  // target: "serverless",
+  webpack: (config, { isServer, webpack }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      // config.node = { fs: 'empty' };
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.electron = false;
+    }
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^electron$/,
+      })
+    );
 
-      return config;
-    },
-    experimental: {
-      externalDir: true,
-      esmExternals: false,
-    },
-  }
-);
-
-
+    return config;
+  },
+  experimental: {
+    externalDir: true,
+    esmExternals: false,
+  },
+});
