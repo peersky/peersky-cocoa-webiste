245564e0c6824a5dbc0de8d8dc70e914436b18e1
Tim Pechersky <tim.pechersky@gmail.com>
2023-03-10 00:32:35 +0800

    update types

--- types/enums.ts
+++ types/enums.ts
@@ -14,3 +14,4 @@ export enum TokenMust {
   BET,
   GIVE,
 }
+
---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export type SupportedChains =
+  | "localhost"
+  | "mumbai"
+  | "polygon"
+  | "ethereum"
+  | "goerli";
+
+export * as typechain from "typechain";
+export * as enums from "./enums";
