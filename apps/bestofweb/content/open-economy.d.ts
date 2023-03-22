6660c358392a4775581d33fbeea87be085accff1
Tim Pechersky <tim.pechersky@gmail.com>
2023-03-22 15:27:23 +0400

    blog post commited

---
+++ apps/bestofweb/content/open-economy.d.ts
@@ -0,0 +1,10 @@
+export { default } from "*.mdx";
+export const meta: {
+  author?: string;
+  date: string;
+  title?: string;
+  description?: string;
+  tags?: string[];
+  image?: string;
+  path: string;
+};
