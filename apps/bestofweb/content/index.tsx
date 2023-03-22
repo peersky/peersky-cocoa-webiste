0ffd61d05995a0e37727d8d0f135d5627b83dcdf
Tim Pechersky <tim.pechersky@gmail.com>
2023-03-22 15:32:33 +0400

    cleaup and adding mising files

--- apps/bestofweb/content/index.tsx
+++ apps/bestofweb/content/index.tsx
@@ -1,3 +1,2 @@
-import * as WhyBlockchain from "./why-blockchain.mdx";
-import * as InOpenEconomyWeTrust from "./in-blockchain-we-trust.mdx";
-export { WhyBlockchain, InOpenEconomyWeTrust };
+import * as OpenEconomyManifest from "./open-economy.mdx";
+export { OpenEconomyManifest };
