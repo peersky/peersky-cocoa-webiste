c2b51abfd726cc4d0fa837646bc77a4a508f9c1f
Tim Pechersky <tim.pechersky@gmail.com>
2023-03-29 22:50:19 +0400

    imporove analytics tag

--- apps/bestofweb/pages/_document.js
+++ apps/bestofweb/pages/_document.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 // const GTAG = process.env.NEXT_PUBLIC_ENGINE_GTAG;
 const GTAG = "G-7M9KTD2D9E";
 
@@ -11,6 +12,13 @@ export default class MyDocument extends Document {
         style={{ width: "100%", height: "100%", fontSize: "16px" }}
       >
         <Head>
+          <Script Script id="google-analytics" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KJLR89H');`}
+          </Script>
           <meta name="theme-color" content="#000000" />
           <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.png" />
@@ -31,7 +39,6 @@ export default class MyDocument extends Document {
       user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
     -->`} */}
           <link rel="manifest" href="/manifest.json" />
-
           <link rel="preconnect" href="https://s3.amazonaws.com" />
           {/* <link rel="preload" as="font" href="/Virgil.woff2" crossOrigin="" /> */}
           {/* <link
@@ -44,25 +51,18 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           /> */}
           <link rel="preconnect" href="https://s3.amazonaws.com" />
-
           {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GTAG}`}
-          ></script>
+        </Head>
+        <body>
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag() {
-                dataLayer.push(arguments);
-              }
-              gtag("js", new Date());
-              gtag("config", "${GTAG}");`,
+         <!-- Google Tag Manager (noscript) -->
+         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJLR89H"
+         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+         <!-- End Google Tag Manager (noscript) -->`,
             }}
           />
-        </Head>
-        <body>
           <Main />
           <NextScript />
         </body>
