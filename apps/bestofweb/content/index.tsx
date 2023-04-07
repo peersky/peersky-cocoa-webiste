3f72e0b07818a9c0dd2970069fa952b29662b31b
Tim Pechersky <tim.pechersky@gmail.com>
2023-04-07 15:23:56 +0200

    setting-safe-enviroment-ru

--- apps/bestofweb/content/index.tsx
+++ apps/bestofweb/content/index.tsx
@@ -1,4 +1,5 @@
 import * as OpenEconomyManifest from "./open-economy.mdx";
 import * as HowAccountsRu from "./how-wallets-made-ru.mdx";
 import * as NFTRBAC from "./nft-rbac.mdx";
-export { OpenEconomyManifest, HowAccountsRu, NFTRBAC };
+import * as SAFEENV from "./setting-safe-enviroment-ru.mdx";
+export { OpenEconomyManifest, HowAccountsRu, NFTRBAC, SAFEENV };
