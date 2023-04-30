d5b993431557d5cd4f08d84fda2f3447acd9c529
Tim Pechersky <tim.pechersky@gmail.com>
2023-04-30 16:00:56 +0800

    aa agenda and semantic token ids articles

--- apps/bestofweb/content/index.tsx
+++ apps/bestofweb/content/index.tsx
@@ -2,4 +2,13 @@ import * as OpenEconomyManifest from "./open-economy.mdx";
 import * as HowAccountsRu from "./how-wallets-made-ru.mdx";
 import * as NFTRBAC from "./nft-rbac.mdx";
 import * as SAFEENV from "./setting-safe-enviroment-ru.mdx";
-export { OpenEconomyManifest, HowAccountsRu, NFTRBAC, SAFEENV };
+import * as PRIVATEAGENDA from "./aa-for-private-proposals.mdx";
+import * as SEMANTICTOKENIDS from "./token-id-matching-to-msg-sig.mdx";
+export {
+  OpenEconomyManifest,
+  HowAccountsRu,
+  NFTRBAC,
+  SAFEENV,
+  PRIVATEAGENDA,
+  SEMANTICTOKENIDS,
+};
