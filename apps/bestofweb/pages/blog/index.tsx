4d9c284e9c77bb48d3cf5d3307e9afae94eab75e
Tim Pechersky <tim.pechersky@gmail.com>
2023-05-01 09:35:06 +0800

    add nice tag filtering

--- apps/bestofweb/pages/blog/index.tsx
+++ apps/bestofweb/pages/blog/index.tsx
@@ -1,21 +1,93 @@
 import { getLayout } from "@peersky/next-web3-chakra/layouts/BlogLayout";
-import { Flex, Spacer, Text, Tag, Heading } from "@chakra-ui/react";
+import { Flex, Spacer, Text, Tag, Heading, Button } from "@chakra-ui/react";
 import RouteButton from "@peersky/next-web3-chakra/components/RouteButton";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import UIContext from "@peersky/next-web3-chakra/providers/UIProvider/context";
+import useAppRouter from "@peersky/next-web3-chakra/hooks/useRouter";
 
 const Blog = (props: any) => {
   const ui = useContext(UIContext);
+  const appRouter = useAppRouter();
+  const [selectedTags, setSelectedTags] = React.useState<string[]>([]);
+  React.useEffect(() => {
+    setSelectedTags(appRouter.query?.tags?.split("&"));
+  }, [appRouter.query?.tags]);
+  console.log("selectedTags", selectedTags);
+  const [displayPosts, setDisplayPosts] = React.useState(props.posts);
+  React.useEffect(() => {
+    console.log("show all posts", selectedTags);
+    if (!selectedTags || (selectedTags.length == 1 && selectedTags[0] == "")) {
+      setDisplayPosts(props.posts);
+    } else {
+      setDisplayPosts(
+        props.posts.filter((_post: any) => {
+          let found = true;
+          selectedTags.forEach((_stag: any) => {
+            if (!_post.tags.includes(_stag)) {
+              found = false;
+            }
+          });
+          return found;
+        })
+      );
+    }
+  }, [selectedTags, props.posts]);
+
+  const [allTags] = React.useState<string[]>(() => {
+    let tags: string[] = [];
+    props.posts.forEach((post: any) => {
+      post.tags.forEach((tag: string) => {
+        if (!tags.includes(tag)) {
+          tags.push(tag);
+        }
+      });
+    });
+    return tags;
+  });
+  React.useEffect(() => {}, [props.posts, allTags]);
+  console.log(allTags);
   return (
     <Flex w="100%" py={8} direction="column">
-      {props.posts
+      <Flex py={2}>
+        {allTags.map((tagName: string) => (
+          <Tag
+            as={Button}
+            variant={"solid"}
+            colorScheme={selectedTags?.includes(tagName) ? "green" : "blue"}
+            key={tagName}
+            onClick={() => {
+              if (selectedTags?.includes(tagName)) {
+                const newTags = [...selectedTags].filter(
+                  (_tag) => _tag !== tagName
+                );
+                appRouter.appendQuery("tags", newTags.join("&"), false, false);
+
+                // const router
+              } else {
+                // setSelectedTags((prevTags: any) => [tagName, ...prevTags]);
+                let _q = "";
+                if (selectedTags?.length > 0 && selectedTags[0] !== "") {
+                  console.log("selectedTags.length", selectedTags.length);
+                  _q += selectedTags;
+                  _q += "&";
+                }
+                _q += tagName;
+                appRouter.appendQuery("tags", _q, false, false);
+              }
+            }}
+            h="24px"
+          >
+            {tagName}
+          </Tag>
+        ))}
+      </Flex>
+      {displayPosts
         .sort((a: any, b: any) => {
           const da: any = new Date(a.date);
           const db: any = new Date(b.date);
           return db - da;
         })
         .map((post: any) => {
-          console.log(post.description);
           return (
             <Flex
               key={post.title}
