8de21a30c26118ee263ffc815510a95555528cec
Tim Pechersky <tim.pechersky@gmail.com>
2025-03-14 14:42:04 +0800

    change fonts

--- apps/bestofweb/theme.js
+++ apps/bestofweb/theme.js
@@ -3,6 +3,10 @@ import _theme from "@peersky/next-web3-chakra/dist/theme/theme";
 import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   ..._theme,
+  fonts: {
+    Headings: `'JetBrains Mono', monospace`,
+    body: `'JetBrains Mono', monospace`,
+  },
   //Here can override library theme items
 });
 
